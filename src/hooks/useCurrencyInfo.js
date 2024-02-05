1634a0400d5e65d336b39de9c37a117b919f318e
Shrinivas Kulkarni <shrinivaskulkarni989@gmail.com>
2024-02-06 00:50:14 +0530

    updated

--- src/hooks/useCurrencyInfo.js
+++ src/hooks/useCurrencyInfo.js
@@ -1,15 +1,14 @@
-import { data } from "autoprefixer";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react"
 
 
-function useCurrencyInfo(currency) {
+function useCurrencyInfo(currency){
     const [data, setData] = useState({})
     useEffect(() => {
         fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
         .then((res) => res.json())
-        .then((res) =>  SVGMetadataElement(res[currency]))
+        .then((res) => setData(res[currency]))
         console.log(data);
-    },[currency])
+    }, [currency])
     console.log(data);
     return data
 }
